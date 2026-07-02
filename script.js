20d506f0a7c020c1bf93726365573c01bff13626
= <=>
2026-07-02 16:32:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,161 @@
+// let arr = [1, 2, 3, 4, 5];
+
+// let newArr = [...arr];
+// newArr.push(6)
+// console.log(arr);
+// console.log(newArr);
+
+// branching
+// if
+// a=10
+
+// if statement
+// if(a>2){
+//     console.log('sotto')
+// }
+// console.log('baire')
+
+// const age = 18
+
+// if else statement
+// if(age>=18){
+//     console.log('tumi vote dite parba')
+// }
+// else{
+//     console.log('tumi akhon o baccha')
+// }
+
+const mark = 80;
+
+// else if statement
+// if (mark >= 80) {
+//     console.log("A+");
+//   } else if (mark >= 70) {
+//     console.log("A");
+//   } else if (mark >= 60) {
+//     console.log("A-");
+//   } else if (mark >= 50) {
+//     console.log("B");
+//   } else if (mark >= 40) {
+//     console.log("C");
+//   } else if (mark >= 33) {
+//     console.log("D");
+//   }
+//  else {
+//   console.log("F");
+// }
+
+// nested if statement
+// if (mark >= 33) {
+//   if (mark >= 80) {
+//     console.log("A+");
+//   } else if (mark >= 70) {
+//     console.log("A");
+//   } else if (mark >= 60) {
+//     console.log("A-");
+//   } else if (mark >= 50) {
+//     console.log("B");
+//   } else if (mark >= 40) {
+//     console.log("C");
+//   } else if (mark >= 33) {
+//     console.log("D");
+//   }
+// } else {
+//   console.log("F");
+// }
+
+// nested if
+// const student = true;
+// const poor = true;
+
+// if(student){
+//     if(poor){
+//         console.log('tumi taka pabe.')
+//     }else{
+//         console.log('tumi gorib na')
+//     }
+// }else{
+//     console.log('tumi student oi na')
+// }
+
+// shorthand if else
+// const lisense = false ? console.log('tumi driving korte parba') : console.log('tumi driving korte parba na')
+
+
+// switch statement
+// switch(mark){
+//     case 80:
+//         console.log('A+')
+//     case 70
+
+// }
+
+// const department_shortForm = "CIVIL"
+
+// switch(department_shortForm){
+//     case "CST":
+//         console.log("Tumra to freelancer.")
+//         break;
+//     case "ENT":
+//         console.log('Tumra to current er khuti thik koro')
+//         break;
+//     case "RAC":
+//         console.log("Freeze ki nosto?")
+//         break
+//     case "FOOD":
+//         console.log("Ranna koro giye jaw")
+//         break;
+
+//     default:
+//         console.log("Tumi to clg er na. jaw vago")
+
+// }
+
+
+// loop
+// for,while,do while
+// initial value
+// condition
+// increment/decrement
+// for(let i = 100; i>=1; i--){
+//     console.log(i)
+// }
+
+// sum of 1 to 100
+// let sum = 0
+// for (let i = 1; i <= 100; i++) {
+//    sum+=i // sum = sum + i
+// }
+
+// 1 theke 100 porjonto bijur songkha gula console korbo
+// for(let i = 1; i<100; i++){
+//     if(i%2==0){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+// const number = 10000
+// for(let i = 1; i<number; i++){
+//     if(i==5){
+//         break
+//     }
+//     console.log(i)
+// }
+
+// console.log(sum)
+
+// while loop
+// let i = 100;
+
+// while(i<100){
+//     i++
+//     console.log(i)
+// }
+
+// do while
+// let i =1
+// do{
+//         i++
+//     console.log(i)
+// }while(i<100)
